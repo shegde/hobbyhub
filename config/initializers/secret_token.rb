c79e6ca291fbca03f62c37fc7f9b024ff6f89372
Shailesh <hegde.shailesh@gmail.com>
2011-06-14 20:39:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hobbyhub::Application.config.secret_token = '07979c96f888ac93b66c7f7c32aa4b864463ba5eb428f7060637984d2814c1b121597e50bdfddcfd3ccf23fd8e8ba21fcd857cb71edaefdd801a9736be428b1d'
